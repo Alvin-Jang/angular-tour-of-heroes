2d3db23d246c193b37c4fc80fc4d352bd5e525cb
Alvin-Jang <jinweizh@sina.cn>
2021-02-19 15:14:12 +0800

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { FormsModule } from "@angular/forms"; //NgModule lives here
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
-import { MessageService } from './message.service';
-import { MessagesComponent } from './messages/messages.component';
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroService } from "./hero.service";
+import { MessageService } from "./message.service";
+import { MessagesComponent } from "./messages/messages.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
   bootstrap: [AppComponent],
   providers: [HeroService, MessageService]
 })
