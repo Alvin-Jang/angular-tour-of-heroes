f6f3b1f156f7b17405f0cbb8590af138e89da4fe
Alvin-Jang <jinweizh@sina.cn>
2021-03-01 17:58:35 +0800

    2 files modified

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
+
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
 
@@ -29,5 +30,9 @@ export class HeroDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 }
