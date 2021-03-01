25472c219b7ff8a4941cdccea4ab2476f03c54bb
Alvin-Jang <jinweizh@sina.cn>
2021-03-01 16:54:20 +0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,12 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+
+    /**
+     * The HttpClientInMemoryWebApiModule intercepts HTTP requests
+     * and returns simulated server responses
+     * Remove it when a real server is ready to receive requests
+     */
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false
     })
