3b801cf7bbbacb121bf61f7f1a8513c09d121fba
Alvin-Jang <jinweizh@sina.cn>
2021-03-02 16:45:05 +0800

    2 files modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CrisisListComponent } from "./crisis-list/crisis-list.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { HeroesComponent } from "./heroes/heroes.component";
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: "heroes", component: HeroesComponent },
   { path: "dashboard", component: DashboardComponent },
   { path: "detail/:id", component: HeroDetailComponent },
+  { path: "crisis-center", component: CrisisListComponent },
   { path: "**", component: PageNotFoundComponent }
 ];
 
