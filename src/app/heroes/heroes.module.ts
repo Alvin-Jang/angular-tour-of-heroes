c0421c36d79038c111fef073d31e806950ff18b7
Alvin-Jang <jinweizh@sina.cn>
2021-03-03 14:30:18 +0800

    2 files modified, 2 added

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { HeroesRoutingModule } from "./heroes-routing.module";
 import { HeroListComponent } from "./heroes/hero-list.component";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, HeroesRoutingModule],
-  declarations: [HeroListComponent, HeroDetailComponent]
+  declarations: [HeroListComponent, HeroDetailComponent, HeroFormComponent]
 })
 export class HeroesModule {}
